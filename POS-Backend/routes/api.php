ab45fab8c81db9f45277885fb9f868ee366b027a
Chinedu Ukpe <cukpe@emgeresources.com>
2022-06-16 16:26:43 +0100

    Authentication

--- POS-Backend/routes/api.php
+++ POS-Backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +16,28 @@ use App\Http\Controllers\ProductsController;
 |
 */
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+});
+
+Route::group([
+    'middleware' => 'auth:api'
+], function () {
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', [ProductsController::class,'index']);
-Route::post('/products', [ProductsController::class,'create']);
-Route::get('/products/{productCode}', [ProductsController::class,'show']);
-Route::patch('/products/{productCode}', [ProductsController::class,'edit']);
-Route::delete('/products/{productCode}', [ProductsController::class,'destroy']);
-
+Route::get('/products', [ProductsController::class, 'index']);
+Route::post('/products', [ProductsController::class, 'create']);
+Route::get('/products/{productCode}', [ProductsController::class, 'show']);
+Route::patch('/products/{productCode}', [ProductsController::class, 'edit']);
+Route::delete('/products/{productCode}', [ProductsController::class, 'destroy']);
